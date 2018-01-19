7659d2ea40cbf78891d8fe32aee6614b373258d1
Mobile Lulz Lulzinson <nathan@c0refailure.com>
2018-01-20 03:15:54 +1100

    Minor Fixes. Added ability to determine installed java version

--- JavaManager/Form1.cs
+++ JavaManager/Form1.cs
@@ -30,18 +30,33 @@ namespace JavaManager
                 return true;
             else if (Environment.GetEnvironmentVariable("JDK_HOME", EnvironmentVariableTarget.Machine) != null)
                 return true;
-            else if (System.IO.File.Exists(@"C:\Program Files\Java"))
-                return true;
-            else if (System.IO.File.Exists(@"C:\ProgramData\Oracle\Java\javapath"))
+            else if (System.IO.Directory.Exists(@"C:\ProgramData\Oracle\Java\javapath"))
+            {
+                if(System.IO.File.Exists(@"C:\ProgramData\Oracle\Java\javapath\java.jar")&&System.IO.File.Exists(@"C:\ProgramData\Oracle\Java\javapath\javaw.jar"));
+                    return true;
+            }
+            else if (System.IO.Directory.Exists(@"C:\Program Files\Java"))
                 return true;
 
             return false;
 
         }
 
+
         private void button1_Click(object sender, EventArgs e)
         {
             textBox1.Text = isJavaInstalled().ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if(System.IO.Directory.Exists(@"C:\Program Files\Java"))
+            {
+                System.IO.DirectoryInfo directoryInfo = new System.IO.DirectoryInfo(@"C:\Program Files\Java");
+                var directory = directoryInfo.GetDirectories();
+                
+                textBox1.Text = directory[directory.Length-1].Name;
+            }
         }
     }
 }
