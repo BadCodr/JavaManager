0d98eea31fcd6d203ad4747cc3c05330a06f8444
Mobile Lulz Lulzinson <nathan@c0refailure.com>
2018-01-24 22:17:50 +1100

    Added various windows GUID's

--- JavaManager/Form1.cs
+++ JavaManager/Form1.cs
@@ -8,6 +8,9 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using Microsoft.Win32;
+using System.Diagnostics;
+
+
 namespace JavaManager
 {
     public partial class Form1 : Form
@@ -15,17 +18,37 @@ namespace JavaManager
         public Form1()
         {
             InitializeComponent();
+            
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            WebBrowserInit();
             Main();
+            
+            webBrowser1.DocumentCompleted += new WebBrowserDocumentCompletedEventHandler(DocoCompleted);
+            webBrowser1.ScriptErrorsSuppressed = true;
+
+            webBrowser1.Navigate(new Uri("https://java.com/inc/BrowserRedirect1.jsp?locale=en"));
+        }
+        
+
+        private void DocoCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            textBox1.Text = webBrowser1.Url.ToString();
+
+
         }
 
         private void Main()
         {
-            if(isJavaInstalled(out bool is64Bit))
-                textBox1.Text = JavaInstallDirectory(is64Bit);
+            bool is64Bit;
+            string version;
+
+
+            if (isJavaInstalled(out is64Bit))
+                textBox1.Text = JavaInstallDirectory(is64Bit, out version);
+            
         }
 
         private bool isJavaInstalled(out bool is64Bit)
@@ -38,7 +61,7 @@ namespace JavaManager
                 is64Bit = true;
                 return true;
             }
-            
+
             else
             {
                 javaRegistryKey = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, RegistryView.Registry64).OpenSubKey(@"SOFTWARE\JavaSoft\Java Runtime Environment");
@@ -47,27 +70,50 @@ namespace JavaManager
                     is64Bit = false;
                     return true;
                 }
-
             }
 
             is64Bit = Environment.Is64BitOperatingSystem;
             return false;
         }
 
+        
 
-
-        public string JavaInstallDirectory(bool is64Bit)
+        private string JavaInstallDirectory(bool is64Bit, out string version)
         {
-
-
             RegistryKey javaRegistryKey = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, (RegistryView)(is64Bit ? 256 : 512)).OpenSubKey("SOFTWARE\\JavaSoft\\Java Runtime Environment");
             string currentVerion = javaRegistryKey.GetValue("CurrentVersion").ToString();
+            version = currentVerion;
 
             javaRegistryKey = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, (RegistryView)(is64Bit ? 256 : 512)).OpenSubKey("SOFTWARE\\JavaSoft\\Java Runtime Environment\\"+currentVerion);
             string installDirectory = javaRegistryKey.GetValue("JavaHome").ToString();
 
             return installDirectory;
         }
+
+        private void WebBrowserInit()
+        {
+            string processName = Process.GetCurrentProcess().ProcessName + ".exe";
+            RegistryKey Regkey = null;
+
+
+            Regkey = Registry.CurrentUser.OpenSubKey(@"SOFTWARE\\Microsoft\\Internet Explorer\\Main\\FeatureControl\\FEATURE_BROWSER_EMULATION", true);
+
+            string processID = Convert.ToString(Regkey.GetValue(processName));
+            
+            if(processID=="8000")
+            {
+                Regkey.Close();
+                return;
+            }
+
+            Regkey.SetValue(processName, unchecked((int)0x1F40), RegistryValueKind.DWord);
+
+            Regkey.Close();
+
+        }
+
+
+
 
         private void button1_Click(object sender, EventArgs e)
         {
@@ -77,6 +123,16 @@ namespace JavaManager
         private void button2_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            
         }
     }
 }
